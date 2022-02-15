c9c04b14b8fe9d334fc74205eb6ad70dc7325b50
Ma Jecille Dizon <18491889@students.ltu.edu.au>
2022-02-15 14:46:59 +1100

    Edited settings.py (CORS_ORIGIN_ALLOW_ALL = True)

--- backend/settings.py
+++ backend/settings.py
@@ -132,7 +132,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-     'https://clever-spence-0e0685.netlify.app/'
-]
+
+#CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000'
+#     'https://clever-spence-0e0685.netlify.app/'
+#]
+
+CORS_ORIGIN_ALLOW_ALL = True
