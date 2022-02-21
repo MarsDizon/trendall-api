6433edfe76c07893b8e57e813ce9918a10e8f585
Ma Jecille Dizon <18491889@students.ltu.edu.au>
2022-02-21 17:03:35 +1100

    Set DEBUG = False

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4t_xca9$=w80$uq357l&itf=xn*khrhim0v8lh!(ed@n8ijw#='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['trendall-api.herokuapp.com']
 
